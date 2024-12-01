4cdef784128d04deeb9985385d951394a3d69a75
Farhan Ishraq Omi <omifarhan@gmail.com>
2024-12-01 13:34:22 +0600

    demo trial and error

--- ocr.py
+++ ocr.py
@@ -12,29 +12,23 @@ kwargs = {}
 
 ## system prompt to use for the vision model
 custom_system_prompt = """
-The pdf page below is the CPCN A901 renewal form for the state of New Jersey.
-I need to get all the information of the applicant from the form. Please make sure to include all the details of the applicant.
-Please provide me with a json file containing the extracted information. While making the json, for the fields in which data is not available, please put "N/A" in the json. For checkboxes, if checked, put true, else false.
-If you are unable to extract any information, please put "N/A" in the json. In response, do not write anything else, just provide the json file.
-
-# """
+Below is an image of a pdf of bank statement. Please make a json file containing all the details in the pdf. Do not miss any details in the pdf. For dates, maintain the format YYYY/MM/DD. In response, do not write anything else, just provide the json file.
+"""
 # custom_system_prompt = None  # Use None for default system prompt
 # to override
 # custom_system_prompt = "For the below pdf page, do something..something..." ## example
 
 ###################### Example for Azure OpenAI ######################
-model = os.getenv("MODEL")
-os.environ["AZURE_API_KEY"] = os.getenv("AZURE_API_KEY")
-os.environ["AZURE_API_BASE"] = os.getenv("AZURE_API_BASE")
-os.environ["AZURE_API_VERSION"] = os.getenv("AZURE_API_VERSION")
+model = "gpt-4o-mini" ## openai model
+os.environ["OPENAI_API_KEY"] = os.getenv("openai_key") ## your-api-key
 
 
 # Define main async entrypoint
 async def main():
-    file_path = "./pdf/Signed A901.pdf" ## local filepath and file URL supported
+    file_path = "./pdf/rohan.pdf" ## local filepath and file URL supported
 
     ## process only some pages or all
-    select_pages = [1,4,7,8] ## None for all, but could be int or list(int) page numbers (1 indexed)
+    select_pages = [1] ## None for all, but could be int or list(int) page numbers (1 indexed)
 
     output_dir = "./output"  # Use a relative path to a directory within your project
 
